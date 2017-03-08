1f317c865f207a89093668c925f7671497739341
reniris <nyoronu@gmail.com>
2017-03-08 19:44:04 +0900

    ボタンを押すと下にメッセージを出す

--- NyanTweet/NyanTweet/ViewModel/MainPageViewModel.cs
+++ NyanTweet/NyanTweet/ViewModel/MainPageViewModel.cs
@@ -2,21 +2,48 @@
 using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace NyanTweet.ViewModel
 {
 	class MainPageViewModel
 	{
-		public ReactiveProperty<string> TweetWord { get; }
+        /// <summary>
+        /// 
+        /// </summary>
+        [Required(ErrorMessage = "エラー")]
+        public ReactiveProperty<string> TweetWord { get; }
 
-		private MainSetting _setting = new MainSetting();
+        public ReactiveProperty<string> Message { get; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public ReactiveCommand TweetCommand { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private MainSetting _setting = new MainSetting();
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
 		public MainPageViewModel()
 		{
-			this.TweetWord = this._setting.TweetWord.ToReactiveProperty();
-		}
+			this.TweetWord = this._setting.TweetWord.ToReactiveProperty().SetValidateAttribute(() => this.TweetWord);
+
+            this.Message = new ReactiveProperty<string>("");
+
+            this.TweetCommand = this.TweetWord.Select(s => 0 < s.Length && s.Length <= 140).ToReactiveCommand();
+
+            this.TweetCommand.Subscribe(_ => this.Message.Value = this.TweetWord.Value);
+
+        }
 	}
 }
