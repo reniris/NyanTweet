a58827a676886052b4c8bc7c4708565530e2779b
reniris <nyoronu@gmail.com>
2017-03-08 19:49:42 +0900

    ボタンを押すとメッセージを出す

---
+++ NyanTweet/NyanTweet/Model/MainSetting.cs
@@ -0,0 +1,23 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NyanTweet.Model
+{
+	class MainSetting
+	{
+		private const string DEF_TWEET = "にゃーん";
+
+		//public string TweetWord { get; set; }
+		public ReactiveProperty<string> TweetWord { get; }
+
+		public MainSetting()
+		{
+			this.TweetWord = new ReactiveProperty<string>(ImmediateScheduler.Instance, DEF_TWEET);
+		}
+	}
+}
---
+++ NyanTweet/NyanTweet/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using NyanTweet.Model;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NyanTweet.ViewModel
+{
+	class MainPageViewModel
+	{
+		public ReactiveProperty<string> TweetWord { get; }
+
+		private MainSetting _setting = new MainSetting();
+
+		public MainPageViewModel()
+		{
+			this.TweetWord = this._setting.TweetWord.ToReactiveProperty();
+		}
+	}
+}
