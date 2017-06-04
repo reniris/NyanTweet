8ed0211012ad24ea7f56b738417dcf180e5b9dce
reniris <nyoronu@gmail.com>
2017-06-05 00:55:36 +0900

    ファイルへのセーブタイミングを変更

--- NyanTweet/NyanTweet/Model/MainSetting.cs
+++ NyanTweet/NyanTweet/Model/MainSetting.cs
@@ -11,48 +11,22 @@ using System.Threading.Tasks;
 
 namespace NyanTweet.Model
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    class MainSetting
-    {
-        /// <summary>
-        /// デフォルトのツイート文字列
-        /// </summary>
-        private const string DEF_TWEET = "にゃーん";
-
-        /// <summary>
-        /// The tweet word
-        /// </summary>
-        private string _TweetWord = DEF_TWEET;
-
-        /// <summary>
-        /// ツイート文字列
-        /// </summary>
-        /// <value>
-        /// The tweet word.
-        /// </value>
-        [DefaultValue(DEF_TWEET)]
-        public string TweetWord
-        {
-            get
-            {
-                return _TweetWord;
-            }
-            set
-            {
-				if (_TweetWord != value)
-				{
-					_TweetWord = value;
-					Save(this);
-				}
-            }
-        }
-		
+	/// <summary>
+	/// 
+	/// </summary>
+	class MainSetting
+	{
 		/// <summary>
-		/// The access token
+		/// デフォルトのツイート文字列
 		/// </summary>
-		private string _AccessToken = null;
+		private const string DEF_TWEET = "にゃーん";
+
+		/// <summary>
+		/// ツイート文字列
+		/// </summary>
+		[DefaultValue(DEF_TWEET)]
+		[JsonProperty]
+		public string TweetWord { get; set; } = DEF_TWEET;
 
 		/// <summary>
 		/// Gets or sets the access token.
@@ -61,16 +35,7 @@ namespace NyanTweet.Model
 		/// The access token.
 		/// </value>
 		[JsonProperty]
-		public string AccessToken
-		{
-			get { return _AccessToken; }
-			private set { _AccessToken = value; }
-		}
-
-		/// <summary>
-		/// The access token secret
-		/// </summary>
-		private string _AccessTokenSecret = null;
+		public string AccessToken { get; private set; } = null;
 
 		/// <summary>
 		/// Gets or sets the access token secret.
@@ -79,19 +44,28 @@ namespace NyanTweet.Model
 		/// The access token secret.
 		/// </value>
 		[JsonProperty]
-		public string AccessTokenSecret
-		{
-			get { return _AccessTokenSecret; }
-			private set { _AccessTokenSecret = value; }
-		}
+		public string AccessTokenSecret { get; private set; } = null;
 
 		/// <summary>
 		/// Prevents a default instance of the <see cref="MainSetting"/> class from being created.
 		/// </summary>
 		private MainSetting()
-        {
+		{
 
-        }
+		}
+
+		/// <summary>
+		/// ツイート文をセット
+		/// </summary>
+		/// <param name="text">ツイート文</param>
+		public void SetTweetWord(string text)
+		{
+			if (text == this.TweetWord)
+				return;
+
+			this.TweetWord = text;
+			Save(this);
+		}
 
 		/// <summary>
 		/// アクセストークンをセット
@@ -126,36 +100,36 @@ namespace NyanTweet.Model
 			task.Wait();
 		}
 
-        /// <summary>
-        /// JSONに書き出す
-        /// </summary>
-        /// <param name="data">書き出すデータ</param>
-        static private string SaveJson(MainSetting data)
-        {
-            string json = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+		/// <summary>
+		/// JSONに書き出す
+		/// </summary>
+		/// <param name="data">書き出すデータ</param>
+		static private string SaveJson(MainSetting data)
+		{
+			string json = Newtonsoft.Json.JsonConvert.SerializeObject(data);
 			return json;
-        }
-        
-        /// <summary>
-        /// ユーザーデータをファイルに書き出す
-        /// </summary>
-        /// <param name="text">書き出す文字列</param>
-        /// <returns></returns>
-        static private async Task<string> SaveTextAsync(string text)
-        {
-            // ユーザーデータ保存フォルダー
-            PCLStorage.IFolder localFolder = PCLStorage.FileSystem.Current.LocalStorage;
+		}
 
-            // ファイルを作成、または、取得する
-            PCLStorage.IFile file
-              = await localFolder.CreateFileAsync(TextFileName,
-                                  PCLStorage.CreationCollisionOption.ReplaceExisting).ConfigureAwait(false);
+		/// <summary>
+		/// ユーザーデータをファイルに書き出す
+		/// </summary>
+		/// <param name="text">書き出す文字列</param>
+		/// <returns></returns>
+		static private async Task<string> SaveTextAsync(string text)
+		{
+			// ユーザーデータ保存フォルダー
+			PCLStorage.IFolder localFolder = PCLStorage.FileSystem.Current.LocalStorage;
 
-            // テキストをファイルに書き込む
-            await file.WriteAllTextAsync(text).ConfigureAwait(false);
+			// ファイルを作成、または、取得する
+			PCLStorage.IFile file
+			  = await localFolder.CreateFileAsync(TextFileName,
+								  PCLStorage.CreationCollisionOption.ReplaceExisting).ConfigureAwait(false);
 
-            return file.Path;
-        }
+			// テキストをファイルに書き込む
+			await file.WriteAllTextAsync(text).ConfigureAwait(false);
+
+			return file.Path;
+		}
 
 		/// <summary>
 		/// データをロード
@@ -165,42 +139,42 @@ namespace NyanTweet.Model
 		{
 			var jsonstring = LoadTextAsync();
 			MainSetting data = LoadJson(jsonstring.Result);
-			if(data == null)
+			if (data == null)
 				data = new MainSetting();
 
 			return data;
 		}
 
-        /// <summary>
-        /// JSONからロード
-        /// </summary>
-        /// <returns></returns>
-        static private MainSetting LoadJson(string jsonstring)
-        {
-            MainSetting data = null;
-            if (String.IsNullOrEmpty(jsonstring) == false)
-                data = JsonConvert.DeserializeObject<MainSetting>(jsonstring);
+		/// <summary>
+		/// JSONからロード
+		/// </summary>
+		/// <returns></returns>
+		static private MainSetting LoadJson(string jsonstring)
+		{
+			MainSetting data = null;
+			if (String.IsNullOrEmpty(jsonstring) == false)
+				data = JsonConvert.DeserializeObject<MainSetting>(jsonstring);
 
 			return data;
-        }
-        
-        /// <summary>
-        /// ユーザーデータをファイルから読み取る
-        /// </summary>
-        /// <returns></returns>
-        static private async Task<string> LoadTextAsync()
-        {
-            // ユーザーデータ保存フォルダー
-            PCLStorage.IFolder localFolder = PCLStorage.FileSystem.Current.LocalStorage;
+		}
 
-            ExistenceCheckResult res = await localFolder.CheckExistsAsync(TextFileName).ConfigureAwait(false);
-            if (res == ExistenceCheckResult.NotFound) { return null; }
+		/// <summary>
+		/// ユーザーデータをファイルから読み取る
+		/// </summary>
+		/// <returns></returns>
+		static private async Task<string> LoadTextAsync()
+		{
+			// ユーザーデータ保存フォルダー
+			PCLStorage.IFolder localFolder = PCLStorage.FileSystem.Current.LocalStorage;
 
-            // ファイルを取得する
-            PCLStorage.IFile file = await localFolder.GetFileAsync(TextFileName).ConfigureAwait(false);
+			ExistenceCheckResult res = await localFolder.CheckExistsAsync(TextFileName).ConfigureAwait(false);
+			if (res == ExistenceCheckResult.NotFound) { return null; }
 
-            // テキストファイルを読み込む
-            return await file.ReadAllTextAsync().ConfigureAwait(false);
-        }
-    }
+			// ファイルを取得する
+			PCLStorage.IFile file = await localFolder.GetFileAsync(TextFileName).ConfigureAwait(false);
+
+			// テキストファイルを読み込む
+			return await file.ReadAllTextAsync().ConfigureAwait(false);
+		}
+	}
 }
--- NyanTweet/NyanTweet/ViewModel/MainPageViewModel.cs
+++ NyanTweet/NyanTweet/ViewModel/MainPageViewModel.cs
@@ -15,87 +15,87 @@ using Xamarin.Forms;
 namespace NyanTweet.ViewModel
 {
 	class MainPageViewModel : IDisposable
-    {
-        /// <summary>
-        /// ツイートする文
-        /// </summary>
-        [Required(ErrorMessage = "エラー")]
-        public ReactiveProperty<string> TweetWord { get; }
+	{
+		/// <summary>
+		/// ツイートする文
+		/// </summary>
+		[Required(ErrorMessage = "エラー")]
+		public ReactiveProperty<string> TweetWord { get; }
 
-        /// <summary>
-        /// メッセージテキスト
-        /// </summary>
-        public ReactiveProperty<string> Message { get; }
+		/// <summary>
+		/// メッセージテキスト
+		/// </summary>
+		public ReactiveProperty<string> Message { get; }
 
-        /// <summary>
-        /// ツイート文設定エリアの表示・非表示
-        /// </summary>
-        public ReactiveProperty<bool> IsVisibleSetting { get; }
+		/// <summary>
+		/// ツイート文設定エリアの表示・非表示
+		/// </summary>
+		public ReactiveProperty<bool> IsVisibleSetting { get; }
 
-        /// <summary>
-        /// ツイート文設定エリア
-        /// </summary>
-        public ReactiveProperty<string> SettingText { get; }
+		/// <summary>
+		/// ツイート文設定エリア
+		/// </summary>
+		public ReactiveProperty<string> SettingText { get; }
 
-        /// <summary>
-        /// 設定ボタン表示文
-        /// </summary>
-        public ReactiveProperty<string> SettingButtonText { get; }
+		/// <summary>
+		/// 設定ボタン表示文
+		/// </summary>
+		public ReactiveProperty<string> SettingButtonText { get; }
 
-        /// <summary>
-        /// ツイート用コマンド
-        /// </summary>
-        public ReactiveCommand TweetCommand { get; }
+		/// <summary>
+		/// ツイート用コマンド
+		/// </summary>
+		public ReactiveCommand TweetCommand { get; }
 
-        /// <summary>
-        /// ツイート文設定コマンド
-        /// </summary>
-        public ReactiveCommand SettingCommand { get; }
+		/// <summary>
+		/// ツイート文設定コマンド
+		/// </summary>
+		public ReactiveCommand SettingCommand { get; }
 
-        /// <summary>
-        /// 設定用クラス
-        /// </summary>
-        private MainSetting _setting = MainSetting.Load();
+		/// <summary>
+		/// 設定用クラス
+		/// </summary>
+		private MainSetting _setting = MainSetting.Load();
 
-        /// <summary>
-        /// ツイート用クラス
-        /// </summary>
-        private Tweet _tweet = new Tweet();
+		/// <summary>
+		/// ツイート用クラス
+		/// </summary>
+		private Tweet _tweet = new Tweet();
 
-        /// <summary>
-        /// 設定ボタン表示文のリスト
-        /// </summary>
-        private static readonly string[] SettingButtonTextList = new string[] { "設定", "確定" };
-        
-        /// <summary>
-        /// 設定ボタン表示文のインデックス
-        /// </summary>
-        private int SettingButtonTextIndex = 0;
+		/// <summary>
+		/// 設定ボタン表示文のリスト
+		/// </summary>
+		private static readonly string[] SettingButtonTextList = new string[] { "設定", "確定" };
 
-        /// <summary>
-        /// 
-        /// </summary>
-        private CompositeDisposable Disposable { get; } = new CompositeDisposable();
+		/// <summary>
+		/// 設定ボタン表示文のインデックス
+		/// </summary>
+		private int SettingButtonTextIndex = 0;
 
-        /// <summary>
-        /// コンストラクタ
-        /// </summary>
+		/// <summary>
+		/// 
+		/// </summary>
+		private CompositeDisposable Disposable { get; } = new CompositeDisposable();
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
 		public MainPageViewModel()
-        {
-            //プロパティの初期化
-            this.TweetWord = ReactiveProperty.FromObject(this._setting, x => x.TweetWord);
-            this.SettingText = new ReactiveProperty<string>(this.TweetWord.Value);
-            this.Message = new ReactiveProperty<string>("");
-            this.IsVisibleSetting = new ReactiveProperty<bool>(false);
-            this.SettingButtonText = new ReactiveProperty<string>(SettingButtonTextList[SettingButtonTextIndex]);
+		{
+			//プロパティの初期化
+			this.TweetWord = ReactiveProperty.FromObject(this._setting, x => x.TweetWord);
+			this.SettingText = new ReactiveProperty<string>(this.TweetWord.Value);
+			this.Message = new ReactiveProperty<string>("");
+			this.IsVisibleSetting = new ReactiveProperty<bool>(false);
+			this.SettingButtonText = new ReactiveProperty<string>(SettingButtonTextList[SettingButtonTextIndex]);
 
 			//コマンドの初期化
-			this.TweetCommand = this.TweetWord.Select(s => IsTweet(s)).ToReactiveCommand().AddTo(this.Disposable);			
+			this.TweetCommand = this.TweetWord.Select(s => IsTweet(s)).ToReactiveCommand().AddTo(this.Disposable);
 			this.SettingCommand = new ReactiveCommand().AddTo(this.Disposable);
 
-            //ツイートコマンドの実装
-            this.TweetCommand.Subscribe(async _ =>
-            {
+			//ツイートコマンドの実装
+			this.TweetCommand.Subscribe(async _ =>
+			{
 				//トークン初期化（すでにトークンが存在する場合は何もしない）
 				var init = await _tweet.InitTokenAsync(this._setting.AccessToken, this._setting.AccessTokenSecret);
 				//ツイート
@@ -103,23 +103,27 @@ namespace NyanTweet.ViewModel
 				//アクセストークンをセーブ（変更がない場合はファイルに書き込まれません）
 				this._setting.SetAccessToken(_tweet.Token.AccessToken, _tweet.Token.AccessTokenSecret);
 
-				if (res != null)    
+				if (res != null)
 					this.Message.Value = this.TweetWord.Value;          //メッセージ欄にツイート文を表示
 				else
-					this.Message.Value = this._tweet.LastErrMessage;	//エラーのときはエラーメッセージを表示
+					this.Message.Value = this._tweet.LastErrMessage;    //エラーのときはエラーメッセージを表示
 			});
-			
+
 			//ツイート文設定コマンドの実装
 			this.SettingCommand.Subscribe(_ =>
-            {
-                //ツイート文を変更
-                this.TweetWord.Value = this.SettingText.Value;
-                //ツイート文設定エリアの表示切替
-                this.IsVisibleSetting.Value = !this.IsVisibleSetting.Value;
+			{
+				//ファイルに設定をセーブ
+				if (this.SettingText.Value != this.TweetWord.Value)
+					MainSetting.Save(this._setting);
 
-                //ボタンテキスト設定、確定の切り替え
-                this.SettingButtonText.Value = SettingButtonTextList[SettingButtonTextIndex ^= 1];
-            });
+				//ツイート文を変更
+				this.TweetWord.Value = this.SettingText.Value;
+				//ツイート文設定エリアの表示切替
+				this.IsVisibleSetting.Value = !this.IsVisibleSetting.Value;
+
+				//ボタンテキスト設定、確定の切り替え
+				this.SettingButtonText.Value = SettingButtonTextList[SettingButtonTextIndex ^= 1];
+			});
 		}
 
 		/// <summary>
@@ -128,19 +132,19 @@ namespace NyanTweet.ViewModel
 		/// <param name="tweet"></param>
 		/// <returns></returns>
 		private static bool IsTweet(string tweet)
-        {
+		{
 			if (string.IsNullOrEmpty(tweet) == true)
 				return false;
 
-            return 0 < tweet.Length && tweet.Length <= 140;
-        }
+			return 0 < tweet.Length && tweet.Length <= 140;
+		}
 
-        /// <summary>
-        /// Dispose
-        /// </summary>
-        public void Dispose()
-        {
+		/// <summary>
+		/// Dispose
+		/// </summary>
+		public void Dispose()
+		{
 			this.Disposable.Dispose();
-        }
-    }
+		}
+	}
 }
